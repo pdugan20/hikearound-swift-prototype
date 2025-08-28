b43c996c6751060af4b935e63cfabbe4d8325e25
Patrick Dugan <dugan.pat@gmail.com>
2025-08-28 09:26:36 -0700

    Initial Commit

---
+++ hikearound-prototype/hikearound_prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikearound_prototypeApp.swift
+//  hikearound-prototype
+//
+//  Created by Patrick Dugan on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct hikearound_prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
